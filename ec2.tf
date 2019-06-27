60f6443f58fe536b25139932497bcf7f6a6295e4
Ajoy Bharath <ajoybharath@gmail.com>
2019-06-27 12:31:27 +0530

    Few changes

--- ec2.tf
+++ ec2.tf
@@ -4,26 +4,30 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon-linux-2" {
+  owners      = ["amazon"]
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    values = ["amzn2-ami-hvm*"]
+  }
+
+ filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["099720109477"]
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.my_web_key.key_name}"
+  ami             = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type   = "t2.micro"
+ key_name = "${aws_key_pair.my_web_key.key_name}"
 
   security_groups = [
     "${aws_security_group.allow_ssh.name}",
@@ -31,7 +35,22 @@ resource "aws_instance" "web" {
 ]
 
   tags {
-    Name = "HelloWorld"
+    Name = "WebApp"
+  }
+
+  provisioner "remote-exec" {
+    inline = ["sudo yum -y update"]
+
+    connection {
+      host  = "${self.public_ip}" 
+      type      = "ssh"
+      user      = "ec2-user"
+      private_key = "${file(var.ssh_private_key)}"
+    }
+  }
+
+  provisioner "local-exec" {
+      command = "ansible-playbook -u ec2-user -i '${self.public_ip},' --private-key ${var.ssh_key} docker.yml" 
   }
 }
 output "ip"{
--- vars.tf
+++ vars.tf
@@ -8,3 +8,7 @@ variable "secret_key" {
     description = "Your AWS Secret Key"
     default = "<copy-your-secret-key-here>"
 }
+
+variable "ssh_key" {
+    default = "/home/akbharat/.ssh/my_aws_key"
+}
